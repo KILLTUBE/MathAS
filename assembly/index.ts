e3f40c1433a919629da57e9323a0db465c8a5a86
kungfooman <lama12345@gmail.com>
2019-02-14 03:51:14 +0000

    circumvent asc bug by just using export in assembly/index.ts

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,16 +1,9 @@
-// The entry file of your WebAssembly module.
-
-//import "allocator/arena";
 import "allocator/tlsf";
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
-}
-
-export class Adder {
-	sum: number = 0;
-	addOne(): number {
-		this.sum += 1.0;
-		return this.sum;
-	}
-}
+export {Mat3} from "./Mat3"
+export {Mat4} from "./Mat4"
+//export {pc_math} from "./Math"
+export {Quat} from "./Quat"
+export {Vec2} from "./Vec2"
+export {Vec3} from "./Vec3"
+export {Vec4} from "./Vec4"
