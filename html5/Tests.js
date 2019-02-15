d0352eb79f49ecd24515c3bfdb83e13e236eba92
kungfooman <lama12345@gmail.com>
2019-02-15 05:34:50 +0000

    refactor <as-test> system

--- html5/Tests.js
+++ html5/Tests.js
@@ -13,44 +13,50 @@ function textarea_eval(textarea) {
 }
 
 function tests_init() {
-	tests = document.getElementsByClassName("tests");
+	var tests = document.getElementsByTagName("as-test");
 	for (var test of tests) {
-		test_table = test.children[0];
-		test_tbody = test_table.children[0];
-		for (var test_tr of test_tbody.children) {
-			if (test_tr.children.length != 1) {
-				continue;
-			}
-			var td_code = test_tr.children[0];
-			var td_expect = test_tr.children[1];
-			var code = td_code.innerHTML;
-			td_code.innerHTML = "";
-			code = code.replace(/\r\n/g, '\n');
-			code = code.replace(/^\t+/mg, ''); // remove all tabs from line beginnings (todo: keep intendation)
-			code = code.replace(/^\n/mg, ''); // remove empty lines
-			var textarea = document.createElement("textarea");
-			textarea.value = code;
-			textarea_fit_text(textarea);
-			textarea_editorize(textarea);
-			td_code.appendChild(textarea);
-			
-			var evaled_td = document.createElement("td");
-			test_tr.appendChild(evaled_td);
-			
-			
-			var eval_td = document.createElement("td");
-			var eval_button = document.createElement("button");
-			eval_td.appendChild(eval_button);
-			test_tr.appendChild(eval_td);
-			eval_button.innerText = ">";
-			eval_button.onclick = function(e) {
-				var textarea = this.textarea;
-				var evaled_td = this.evaled_td;
-				evaled_td.innerHTML = textarea_eval(textarea);
-			}.bind({textarea, evaled_td});
-			
-			// same as in onclick, todo: refactor into nice class
+	
+	
+		var code = test.innerHTML;
+		test.innerHTML = "";
+		
+		code = code.replace(/\r\n/g, '\n');
+		code = code.replace(/^\t+/mg, ''); // remove all tabs from line beginnings (todo: keep intendation)
+		code = code.replace(/^\n/mg, ''); // remove empty lines
+		
+		var table    = document.createElement("table");
+		var tr       = document.createElement("tr");
+		var td_a     = document.createElement("td");
+		var td_b     = document.createElement("td");
+		var td_c     = document.createElement("td");
+		var textarea = document.createElement("textarea");
+		var button   = document.createElement("button");
+		
+		// oh wey
+		test .appendChild(table   );
+		table.appendChild(tr      );
+		tr   .appendChild(td_a    );
+		tr   .appendChild(td_b    );
+		tr   .appendChild(td_c    );
+		td_a .appendChild(textarea); // textarea left
+		td_c .appendChild(button  ); // button right, output is middle one (td_b)
+		
+		td_a.classList.add("testcode");
+		td_b.classList.add("testresult");
+		td_c.classList.add("testbutton");
+		
+		textarea.value = code;
+		textarea_fit_text(textarea);
+		textarea_editorize(textarea);		
+		
+		button.innerText = ">";
+		button.onclick = function(e) {
+			var textarea = this.textarea;
+			var evaled_td = this.evaled_td;
 			evaled_td.innerHTML = textarea_eval(textarea);
-		}	
+		}.bind({textarea, td_b});
+		
+		// same as in onclick, todo: refactor into nice class
+		td_b.innerHTML = textarea_eval(textarea);
 	}
 }
