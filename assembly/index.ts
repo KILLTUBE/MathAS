0b069b7debce3499e489fed1026e56ab06dce2e5
kungfooman <lama12345@gmail.com>
2020-06-26 11:12:13 +0200

    Update to latest AS version

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,15 +1,10 @@
-
-//import "allocator/tlsf";
-import "allocator/arena";
-export {memory};
-
-export {Mat3} from "./Mat3"
-export {Mat4} from "./Mat4"
-//export {pc_math} from "./Math"
-export {Quat} from "./Quat"
-export {Vec2} from "./Vec2"
-export {Vec3} from "./Vec3"
-export {Vec4} from "./Vec4"
+export {Mat3} from "./Mat3";
+export {Mat4} from "./Mat4";
+//export {pc_math} from "./Math";
+export {Quat} from "./Quat";
+export {Vec2} from "./Vec2";
+export {Vec3} from "./Vec3";
+export {Vec4} from "./Vec4";
 export {
 	AnimationKeyableNum,
 	AnimationKeyableVec,
@@ -17,4 +12,4 @@ export {
 	AnimationKeyableNumCubicSpline,
 	AnimationKeyableVecCubicSpline,
 	AnimationKeyableQuatCubicSpline
-} from "./AnimationKeyable"
+} from "./AnimationKeyable";
